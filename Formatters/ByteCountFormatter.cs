b475e6bc0f01d4ae032d74d586806e08985d9b93
a-isaiahharvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-07-22 14:19:57 -0400

    first commit

---
+++ Formatters/ByteCountFormatter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskDetect.Formatters
+{
+    public class ByteCountFormatter
+    {
+        public enum ByteCountFormatterUnits
+        {
+            UseBytes = 0,
+            UseKB = 1,
+            UseMB = 2,
+            UseGB = 3,
+            UseTB = 4,
+            UsePB = 5,
+            UseEB = 6,
+            UseAll = 9,
+        }
+
+        public List<ByteCountFormatterUnits> AllowedUnits { get; set; } = new List<ByteCountFormatterUnits>();
+
+        public string stringFromByteCount(long byteCount)
+        {
+            var allowedUnits = new List<ByteCountFormatterUnits>();
+
+            if (AllowedUnits.Count == 0 || AllowedUnits.Contains(ByteCountFormatterUnits.UseAll))
+            {
+                allowedUnits.Add(ByteCountFormatterUnits.UseBytes);
+                allowedUnits.Add(ByteCountFormatterUnits.UseKB);
+                allowedUnits.Add(ByteCountFormatterUnits.UseMB);
+                allowedUnits.Add(ByteCountFormatterUnits.UseGB);
+                allowedUnits.Add(ByteCountFormatterUnits.UseTB);
+                allowedUnits.Add(ByteCountFormatterUnits.UsePB);
+                allowedUnits.Add(ByteCountFormatterUnits.UseEB);
+            }
+            else
+            {
+                allowedUnits.AddRange(AllowedUnits);
+            }
+
+            var unitStr = "bytes";
+            long bytes = byteCount;
+
+            if (AllowedUnits.Contains(ByteCountFormatterUnits.UseBytes))
+            {
+                unitStr = byteCount != 1 ? "bytes" : "byte";
+            }
+            else if (AllowedUnits.Contains(ByteCountFormatterUnits.UseKB))
+            {
+                unitStr = "KB";
+                bytes /= (long)Math.Pow(10, 3);
+            }
+            else if (AllowedUnits.Contains(ByteCountFormatterUnits.UseMB))
+            {
+                unitStr = "MB";
+                bytes /= (long)Math.Pow(10, 6);
+            }
+            else if (AllowedUnits.Contains(ByteCountFormatterUnits.UseGB))
+            {
+                unitStr = "GB";
+                bytes /= (long)Math.Pow(10, 9);
+            }
+            else if (AllowedUnits.Contains(ByteCountFormatterUnits.UseTB))
+            {
+                unitStr = "TB";
+                bytes /= (long)Math.Pow(10, 12);
+            }
+            else if (AllowedUnits.Contains(ByteCountFormatterUnits.UsePB))
+            {
+                unitStr = "PB";
+                bytes /= (long)Math.Pow(10, 15);
+            }
+            else if (AllowedUnits.Contains(ByteCountFormatterUnits.UseEB))
+            {
+                unitStr = "EB";
+                bytes /= (long)Math.Pow(10, 18);
+            }
+            else
+            {
+                // No allowed units present, choose the closest unit based on byteCount
+                var unitsInBytes = new Dictionary<ByteCountFormatterUnits, long>
+        {
+            { ByteCountFormatterUnits.UseBytes, 1 },
+            { ByteCountFormatterUnits.UseKB, (long)Math.Pow(10, 3) },
+            { ByteCountFormatterUnits.UseMB, (long)Math.Pow(10, 6) },
+            { ByteCountFormatterUnits.UseGB, (long)Math.Pow(10, 9) },
+            { ByteCountFormatterUnits.UseTB, (long)Math.Pow(10, 12) },
+            { ByteCountFormatterUnits.UsePB, (long)Math.Pow(10, 15) },
+            { ByteCountFormatterUnits.UseEB, (long)Math.Pow(10, 18) }
+        };
+
+                long closestValue = long.MaxValue;
+
+                foreach (var unit in allowedUnits)
+                {
+                    if (unitsInBytes.ContainsKey(unit))
+                    {
+                        var value = unitsInBytes[unit];
+                        if (Math.Abs(byteCount - value) < Math.Abs(byteCount - closestValue))
+                        {
+                            closestValue = value;
+                            unitStr = unit.ToString().Substring(3);
+                        }
+                    }
+                }
+
+                if (closestValue != long.MaxValue)
+                {
+                    bytes /= closestValue;
+                }
+            }
+
+            return $"{bytes} {unitStr}";
+        }
+    }
+}
---
+++ Models/DiskData.cs
@@ -0,0 +1,84 @@
+﻿using DiskDetect.Formatters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskDetect.Models
+{
+    public class DiskData
+    {
+        private ByteCountFormatter _formatter = new ByteCountFormatter();
+
+        public string Type { get; set; }
+
+        public string Format { get; set; }
+
+        private long _totalFreeSpace { get; set; }
+
+        public string TotalFreeSpace
+        {
+            get
+            {
+                return _formatter.stringFromByteCount(_totalFreeSpace);
+            }
+        }
+
+        private long _availableFreeSpace { get; set; }
+
+        public string AvailableFreeSpace
+        {
+            get
+            {
+                return _formatter.stringFromByteCount(_availableFreeSpace);
+            }
+        }
+
+
+        public string RootDirectory { get; set; }
+
+        private long _totalSize { get; set; }
+
+        public string TotalSize
+        {
+            get
+            {
+                return _formatter.stringFromByteCount(_totalSize);
+            }
+        }
+
+        public string VolumeLabel { get; set; }
+
+        public long Usage
+        {
+            get { return (long)(((double)(_totalSize - _totalFreeSpace) / (double)_totalSize) * 100); }
+        }
+
+        public DiskData(string type, string format, long totalFreeSpace, long availableFreeSpace, string rootDirectory, long totalSize, string volumeLabel)
+        {
+            Type = type;
+            Format = format;
+            _totalFreeSpace = totalFreeSpace;
+            RootDirectory = rootDirectory;
+            _availableFreeSpace = availableFreeSpace;
+            _totalSize = totalSize;
+            VolumeLabel = volumeLabel;
+
+        }
+
+        public static List<DiskData> GetDiskData()
+        {
+            var result = new List<DiskData>();
+            var drives = DriveInfo.GetDrives();
+
+            foreach (var drive in drives)
+            {
+                result.Add(new DiskData(drive.DriveType.ToString(), drive.DriveFormat, drive.TotalFreeSpace, drive.AvailableFreeSpace, drive.RootDirectory.ToString(), drive.TotalSize, drive.VolumeLabel));
+            }
+
+            return result;
+        }
+    }
+}
